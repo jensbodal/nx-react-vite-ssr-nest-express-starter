b8dd30f0e9b036d0a83b1dfeb171d6a3fe7609f1
Jens Bodal <jensbodal@gmail.com>
2023-03-31 07:39:50 +0200

    feat: build API using vavite
    * supports esm without .js extensions
    * uses vite dev server for local development
    * builds normal nodeJS application for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import type { Express } from "express";
+import { IncomingMessage, ServerResponse } from "node:http";
 
 import { AppModule } from './app/app.module';
 
@@ -13,10 +10,34 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  if (import.meta.env.PROD) {
+    // For production, start your server
+    // as you would normally do.
+    await app.listen(port);
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+  } else {
+    await app.init();
+    resolveHandler(await app.getHttpAdapter().getInstance());
+  }
 }
 
 bootstrap();
+
+let resolveHandler: (value: Express) => void;
+let expressHandler: Express | Promise<Express> = new Promise((resolve) => {
+	resolveHandler = resolve;
+});
+
+export default async function handler(
+	request: IncomingMessage,
+	reply: ServerResponse,
+) {
+	if (expressHandler instanceof Promise) {
+		expressHandler = await expressHandler;
+	}
+
+	expressHandler(request, reply);
+}
---
+++ apps/api/vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import vavite from 'vavite';
+import { join } from 'node:path';
+import { swc } from 'rollup-plugin-swc3';
+
+export default defineConfig({
+  server: {
+    open: '/api',
+    host: 'localhost',
+    port: 3333,
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: '../../dist/apps/api',
+  },
+  ssr: {
+    external: ['reflect-metadata'],
+  },
+  esbuild: false,
+  plugins: [
+    {
+      ...swc({
+        jsc: {
+          baseUrl: join(__dirname, './src'),
+          paths: {
+            '*': ['*'],
+          },
+          target: 'es2021',
+          transform: {
+            decoratorMetadata: true,
+            legacyDecorator: true,
+          },
+        },
+      }),
+      enforce: 'pre',
+    },
+    vavite({
+      handlerEntry: 'apps/api/src/main.ts',
+      serverEntry: 'apps/api/src/main.ts',
+      serveClientAssetsInDev: true,
+      // Don't reload when dynamically imported dependencies change
+      reloadOn: 'static-deps-change',
+    }),
+  ],
+});
